2026791a53f5cce08e3420c4152fa8ff6b530d22
Lucifer-RI <1756408459@qq.com>
2022-11-20 21:54:04 +0800

    Make try

---
+++ build/CMakeFiles/Maglev.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ysfyuan/MAGLEV/src/Feeds.cc" "/home/ysfyuan/MAGLEV/build/CMakeFiles/Maglev.dir/src/Feeds.cc.o"
+  "/home/ysfyuan/MAGLEV/src/KF.cc" "/home/ysfyuan/MAGLEV/build/CMakeFiles/Maglev.dir/src/KF.cc.o"
+  "/home/ysfyuan/MAGLEV/src/MaglevServer.cc" "/home/ysfyuan/MAGLEV/build/CMakeFiles/Maglev.dir/src/MaglevServer.cc.o"
+  "/home/ysfyuan/MAGLEV/src/Service.cc" "/home/ysfyuan/MAGLEV/build/CMakeFiles/Maglev.dir/src/Service.cc.o"
+  "/home/ysfyuan/MAGLEV/src/ServiceClient.cc" "/home/ysfyuan/MAGLEV/build/CMakeFiles/Maglev.dir/src/ServiceClient.cc.o"
+  "/home/ysfyuan/MAGLEV/src/SystemStatus.cc" "/home/ysfyuan/MAGLEV/build/CMakeFiles/Maglev.dir/src/SystemStatus.cc.o"
+  "/home/ysfyuan/MAGLEV/src/main.cc" "/home/ysfyuan/MAGLEV/build/CMakeFiles/Maglev.dir/src/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Maglev.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Maglev.dir/src/Feeds.cc.o"
+  "CMakeFiles/Maglev.dir/src/ServiceClient.cc.o"
+  "CMakeFiles/Maglev.dir/src/Service.cc.o"
+  "CMakeFiles/Maglev.dir/src/SystemStatus.cc.o"
+  "CMakeFiles/Maglev.dir/src/main.cc.o"
+  "CMakeFiles/Maglev.dir/src/MaglevServer.cc.o"
+  "CMakeFiles/Maglev.dir/src/KF.cc.o"
+  "Maglev.pdb"
+  "Maglev"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Maglev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
